08b60acbbbd7ccca96d1d12f7d1a9e404c7dd547
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-03-03 22:51:21 +0200

    Copy from the part 4 to part 5

---
+++ coursework/part_5/modern_portfolio/dist/js/main.js
@@ -0,0 +1,30 @@
+// Select DOM Items
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNavItems = document.querySelectorAll(".nav-item");
+// Set initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    menuNavItems.forEach((item) => item.classList.add("show"));
+    // Set menu's state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNavItems.forEach((item) => item.classList.remove("show"));
+    // Set menu's state
+    showMenu = false;
+  }
+}
