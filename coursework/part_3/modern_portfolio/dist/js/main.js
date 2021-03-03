d04e031188ff5a2cf250523f02be5e01fdeccee7
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-03-03 20:48:41 +0200

    Get required DOM items' references to constant variables

---
+++ coursework/part_3/modern_portfolio/dist/js/main.js
@@ -0,0 +1,6 @@
+// Select DOM Items
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".btn-menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNavItems = document.querySelectorAll(".nav-items");
