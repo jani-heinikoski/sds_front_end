a8da2e571e10d82e4e3c61708ee66847250331e2
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-06-28 18:16:17 +0300

    Comments, row and column check for the fits function, let keyword for for-loops

--- project/sudoku_solver/dist/js/sudoku.js
+++ project/sudoku_solver/dist/js/sudoku.js
@@ -1,18 +1,8 @@
 /*
 Author: Jani Heinikoski
-Last modified: 18.06.2021
+Last modified: 28.06.2021
 Description: Sudoku game logic
 Sources:*/
-
-/*
-1. Load references for the input elements inside the game grid
-2. Load references for the sudoku's buttons (solve, reset, load ex.)
-3. Register onclicks for the buttons
-    3.1 Solve button tries to solve the prefilled sudoku using a backtracking brute force algorithm (refer to docs)
-    3.2 Reset button removes all digits from the game grid
-    3.3 Load example loads an example sudoku which the algorithm is able to solve fairly efficiently.
-*/
-
 /* ---------------------- GLOBAL VARIABLES ----------------------*/
 // Represents the Sudoku as an 2d-array consisting of the input elements
 const GAME_GRID = [];
@@ -29,28 +19,47 @@ const EXAMPLE_GAME = [
     [],
 ];
 /* ---------------------- UTILITY FUNCTIONS ----------------------*/
-// Checks if the number at gameGrid[row][col] fits 
+// Checks if the number at gameGrid[row][col] fits as per Sudoku's rules
 const fits = (row, col, gameGrid) => {
+    let val = gameGrid[row][col].value;
+    // An empty cell always "fits"
+    if (val === '')
+        return true;
+    // Check if the row has the same number already
+    for (let i = 0; i < 9; i++)
+        if (gameGrid[row][i].value === val && i !== col)
+            return false;
+    // Check if the column has the same number already
+    for (let i = 0; i < 9; i++)
+        if (gameGrid[i][col].value === val && i !== row)
+            return false;
     return true;
 }
 // Checks if the grid has been filled with proper values
 const checkGridSanity = (gameGrid) => {
-    for (i = 0; i < 9; i++) {
-        for (j = 0; j < 9; j++) {
+    for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            // If a cell contains something, it must be a number between 1 and 9
             if (gameGrid[i][j].value !== '' && !(gameGrid[i][j].value <= 9 && gameGrid[i][j].value >= 1))
+                return false;
+            if (!fits(i, j, gameGrid)) 
                 return false;
         }
     }
     return true;
 }
 /* ---------------------- INITIALIZATION ----------------------*/
-// Fetch the game grid's input elements to the global GAME_GRID array
-for (i = 1; i < 10; i++) {
-    GAME_GRID[i - 1] = [];
-    for (j = 1; j < 10; j++) {
-        GAME_GRID[i - 1].push(document.getElementById(`${i}${j}`));
+// Fetches the references of the game grid's input elements to the global GAME_GRID array
+const initializeGameGrid = () => {
+    for (let i = 1; i < 10; i++) {
+        GAME_GRID[i - 1] = [];
+        for (let j = 1; j < 10; j++) {
+            GAME_GRID[i - 1].push(document.getElementById(`${i}${j}`));
+        }
     }
 }
+// Initialize the game grid before adding event listeners to the buttons
+initializeGameGrid();
 // Event listener for the "Solve Button"
 const SOLVE_BTN = document.getElementsByClassName('solve')[0];
 SOLVE_BTN.addEventListener('click', () => {
