19c9d47aea4263cad884b737198eb239007613a9
jani-heinikoski <59805433+jani-heinikoski@users.noreply.github.com>
2021-06-28 16:27:55 +0300

    Comments and code reorganization

--- project/sudoku_solver/dist/js/sudoku.js
+++ project/sudoku_solver/dist/js/sudoku.js
@@ -13,6 +13,9 @@ Sources:*/
     3.3 Load example loads an example sudoku which the algorithm is able to solve fairly efficiently.
 */
 
+/* ---------------------- GLOBAL VARIABLES ----------------------*/
+// Represents the Sudoku as an 2d-array consisting of the input elements
+const GAME_GRID = [];
 // An example sudoku
 const EXAMPLE_GAME = [
     [],
@@ -25,7 +28,11 @@ const EXAMPLE_GAME = [
     [],
     [],
 ];
-
+/* ---------------------- UTILITY FUNCTIONS ----------------------*/
+// Checks if the number at gameGrid[row][col] fits 
+const fits = (row, col, gameGrid) => {
+    return true;
+}
 // Checks if the grid has been filled with proper values
 const checkGridSanity = (gameGrid) => {
     for (i = 0; i < 9; i++) {
@@ -35,23 +42,21 @@ const checkGridSanity = (gameGrid) => {
         }
     }
     return true;
-} 
-
-// Fetch the game grid's input elements to a 2D-array
-const GAME_GRID = [];
+}
+/* ---------------------- INITIALIZATION ----------------------*/
+// Fetch the game grid's input elements to the global GAME_GRID array
 for (i = 1; i < 10; i++) {
     GAME_GRID[i - 1] = [];
     for (j = 1; j < 10; j++) {
-        console.log(`${i}${j}`);
         GAME_GRID[i - 1].push(document.getElementById(`${i}${j}`));
     }
 }
-
+// Event listener for the "Solve Button"
 const SOLVE_BTN = document.getElementsByClassName('solve')[0];
 SOLVE_BTN.addEventListener('click', () => {
     console.log(checkGridSanity(GAME_GRID));
 });
-
+// Event listener for the "Reset Button"
 const RESET_BTN = document.getElementsByClassName('reset')[0];
 RESET_BTN.addEventListener('click', () => {
     GAME_GRID.forEach((arr) => {arr.forEach((element) => {element.value = ''})});
